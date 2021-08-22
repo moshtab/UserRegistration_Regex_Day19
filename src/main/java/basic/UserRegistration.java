1b1c11c95d10042142012972b9c5b42681bcb2ef
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-22 17:39:55 +0530

    Create UserRegistration.java

---
+++ src/main/java/basic/UserRegistration.java
@@ -0,0 +1,32 @@
+package basic.regexdemo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public boolean validateFirstName(String string) {
+		String firstName = string;
+		String regex = "^([A-Z][a-z]{2,})$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(firstName);
+		return m.matches();
+	}
+
+	public boolean validateLastName(String string) {
+		String lastName = string;
+		String regex = "^([A-Z][a-z]{2,})$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(lastName);
+		return m.matches();
+
+	}
+
+	public boolean validateEmailId(String string) {
+		String emailId = string;
+		String regex = "^([a-zA-Z0-90_.+-]+)@([a-z0-9]+).([a-z]{2,4})(.[a-z]{2,4})?$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(emailId);
+		return m.matches();
+	}
+}
